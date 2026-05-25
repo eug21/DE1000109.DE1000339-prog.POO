69af2fed75ed15638c304787919c1eec76106ef0
eug21 <ricciardieugenio70@gmail.com>
2026-05-25 22:12:43 +0200

    gui: aggiunta gestione clienti e lista clienti

---
+++ src/main/java/gui/GestioneClienti.java
@@ -0,0 +1,54 @@
+package gui;
+
+import javax.swing.*;
+import controller.Controller;
+import model.TipoPatente;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class GestioneClienti {
+    private JPanel gestioneClienti;
+    private JComboBox tipopatenteCombo;
+    private JTextField numeroPatenteTextField;
+    private JTextField codiceFiscaleTextField;
+    private JTextField cognomeTextField;
+    private JTextField nomeTextField;
+    private JButton listaClientiButton;
+    private JButton cercaClienteButton;
+    private JButton aggiungiClienteButton;
+
+    public GestioneClienti() {
+        Controller controller = new Controller();
+        for(TipoPatente tipo : TipoPatente.values()){
+            tipopatenteCombo.addItem(tipo);
+        }
+
+        aggiungiClienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String nome = nomeTextField.getText().trim();
+                String cognome =cognomeTextField.getText().trim();
+                String codiceFiscale = codiceFiscaleTextField.getText().trim();
+                String numeroPatente = numeroPatenteTextField.getText().trim();
+                TipoPatente tipoPatente = (TipoPatente) tipopatenteCombo.getSelectedItem();
+
+                try {
+                    controller.registraCliente(nome, cognome, codiceFiscale, tipoPatente, numeroPatente);
+                    JOptionPane.showMessageDialog(null, "Cliente registrato con successo!", "Successo!", JOptionPane.INFORMATION_MESSAGE);
+                } catch (Exception eccezione){
+                    JOptionPane.showMessageDialog(null, "Errore!", "Errore", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+
+
+        listaClientiButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+    }
+}
---
+++ src/main/java/gui/ListaClienti.java
@@ -0,0 +1,57 @@
+package gui;
+
+import controller.Controller;
+import exception.ClienteNonTrovatoException;
+
+import javax.swing.*;
+import javax.swing.table.DefaultTableModel;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ListaClienti {
+    private JPanel listaClienti;
+    private JButton aggiornaListaButton;
+    private JButton eliminaClienteButton;
+    private JTable tabellaClienti;
+
+    public ListaClienti() {
+        Controller controller = new Controller();
+
+        aggiornaListaButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String [] colonne = {"Numero Patente", "Nome", "Cognome", "Codice Fiscale", "Tipo Patente"};
+                Object[][] righe = controller.getTuttiClienti();
+
+                tabellaClienti.setModel(DefaultTableModel(righe, colonne);
+            }
+        });
+
+        eliminaClienteButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                int riga = tabellaClienti.getSelectedRow();
+                if(riga == -1){
+                    JOptionPane.showMessageDialog(null, "Devi selezionare un cliente", "Attenzione",  JOptionPane.WARNING_MESSAGE);
+                    return;
+                }
+                String numeroPatente = (String) tabellaClienti.getValueAt(riga,0);
+                int conferma = JOptionPane.showConfirmDialog(null, "Sei sicuro di volere eliminare il cliente " + numeroPatente +  "?", "Conferma", JOptionPane.YES_NO_OPTION);
+                if(conferma != JOptionPane.YES_OPTION){
+                    return;
+                }
+                try{
+                    controller.eliminaCliente(numeroPatente);
+                    JOptionPane.showMessageDialog(null, "Cliente eliminato con successo", "Cliente eliminato", JOptionPane.INFORMATION_MESSAGE);
+                    aggiornaListaButton.doClick();
+
+                } catch (ClienteNonTrovatoException eccezione){
+                    JOptionPane.showMessageDialog(null, "Il cliente non e' stato trovato", eccezione.getMessage(), JOptionPane.ERROR_MESSAGE);
+                } catch(Exception eccezione){
+                    JOptionPane.showMessageDialog(null, "Errore", eccezione.getMessage(), JOptionPane.ERROR_MESSAGE);
+                }
+
+            }
+        });
+    }
+}
